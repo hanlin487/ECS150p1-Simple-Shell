255f94c12376eb4e8e8e2da7c2f6c4a2f1d481bc
Nilesh Thiagarajan <nilesh.thiagarajan@gmail.com>
2023-04-17 00:21:20 -0700

    Big data struct update -- allready tested and they work jus need to implement now

--- p1/sshell.c
+++ p1/sshell.c
@@ -8,29 +8,121 @@
 
 #define CMDLINE_MAX 512
 
-//Output redirection, cd, and parsing all work
 
-struct list {
-    char** str_arr;
-    char* file; //for output redirection
+struct node {
+    bool read_signal;
+    bool write_signal;
+    char* file;
+    char** command;
+    struct node* next;
     int length;
 };
 
-//you cant use strlen on a double pointer so I added this struct to keep track of the array length
+//reworking data structures update***
 
-struct list* parse(char* string){
-    struct list *l = malloc(sizeof(struct list));
-    char temp[CMDLINE_MAX];
+
+struct node* createNode(void){
+    struct node *n = malloc(sizeof(struct node));
+    n -> read_signal = false;
+    n -> write_signal = false;
+    n -> file = (char*) malloc(32);
+    n -> command = (char**) malloc(16*sizeof(char*));
+    for (int i = 0; i < 16; i++){
+	n -> command[i] = (char*) malloc(32);
+    }
+
+    n -> file = (char*) malloc(32);
+    
+
+
+    n -> next = NULL;
+    n -> length = 0;
+    return(n);
+}
+
+int getLength(struct node* n){
+    return n -> length;
+}
+
+char** getCommand(struct node* n){
+    return n -> command;
+}
+
+
+
+
+
 
     
-    l -> str_arr = (char**)malloc(16 * sizeof(char *));
+
+
+struct list {
+    struct node* head;
+    struct node* tail;
+    struct node* curr;
+};
+
+struct list* createList(void){
+    struct list *a = malloc(sizeof(struct list));
+    a -> tail = NULL;
+    a -> head = NULL;
+    a -> curr = NULL;
+    return(a);
+}
+
+void insert(struct list* l, struct node* n){
+    if (l -> tail == NULL){
+	l -> tail = n;
+	l -> head = n;
+	n -> next = NULL;
+	return;
+    }
+    l -> tail -> next = n;
+    n -> next = NULL;
+    l -> tail = n;
+}
+
+void front(struct list* l){
+    if (l -> head == NULL){
+	return;
+    }
+
+    l -> curr = l -> head;
+}
+
+void right(struct list* l){
+    if (l -> curr == NULL){
+	return;
+    }
+    l -> curr = l -> curr -> next;
+}
+
+struct node* view(struct list* l){
+    return l -> curr;
+}
+
+
+
+
+
+//Because this parse is a node function I pretty much have to rewrite the strtok code in the main to delimit by "|"
+
+
+void parse(struct node* a, char* string){
+
+    
+    //struct list *l = malloc(sizeof(struct list));
+    char temp[CMDLINE_MAX];
+    
+
+    
+    a -> command = (char**)malloc(16 * sizeof(char *));
     for (int i = 0; i < 16; i++){
-	l -> str_arr[i] = (char*)malloc(32);
+	a -> command[i] = (char*)malloc(32);
     }
     strcpy(temp,string);
     char* ptr;
     int c = 0;
-    l -> file = (char*)malloc(32);
     ptr = strtok(temp, " ");
     while (ptr != NULL){
 	//printf("%s\n",ptr);
@@ -38,78 +130,141 @@ struct list* parse(char* string){
 	if (strcmp(ptr,">") == 0){
 	    ptr = strtok(NULL, " ");
 	    if (ptr != NULL){
-		strcpy(l -> file, ptr);
+		strcpy(a -> file, ptr);
 		ptr = strtok(NULL," ");
 		continue;
 	    }else{
-		l -> file = NULL;
+		a -> file = NULL;
 		break;
 	    }
 
 	}
 	
-	strcpy(l -> str_arr[c],ptr);
+	strcpy(a -> command[c],ptr);
 	ptr = strtok(NULL," ");
 	c += 1;
 	
     }
-    l -> length = c;
+    a -> length = c;
+
 		
 
 
 
 
-    
-    return(l);
 }
 
-int getLength(struct list* l){
-    return l -> length;
+//Ignore this
+
+/*
+
+void pipeline(char **a){
+    //you need to loop through all off the piped calls and create processes = to amount of calls
 }
 
-char** getList(struct list* l){
-    return l -> str_arr;
-}
-
-char* getFile(struct list* l){
-    return l -> file;
-}
-
-
+*/
 
     
+
+
+
+
     
+//Data structures are kinda complicated-ish but heres an example below of how to use them
+
 
 int main(){
 
+
+    	//In an actual terminal this would be inputted as ls -l | more, you could then just delimit the | to get 2 strngs
+	char* string = "ls -l";
+	char* string2 = "more";
+
+	//Create an empty node
+	struct node* a = createNode();
+	//Call parse on that node, the string passed in will be seperated by spaces and turned into a string array of the node (command)
+	//So here, "ls -l" would become an array ["ls","-l"] contained in node a
+	parse(a,string);
+
+
+	int len;
+	char** t;
+
+	//for (int i = 0; i < l ; i++){
+	    //printf("%s\n",t[i]);
+	//}
+
+	//Same thing as node a, but for string2
+	struct node* b = createNode();
+	parse(b,string2);
+	
 	
 
+	//Create a list and then insert both the nodes you just created
+	struct list* l = createList();
+	insert(l,a);
+	insert(l,b);
+
+
+	//Front to move curr to head
+	front(l);
+
+	//View to return curr node
+	struct node* temp;
+	temp = view(l);
+	while (temp != NULL){
+	    //get length and string array of node
+	    len = getLength(temp);
+	    t = getCommand(temp);
+
+	    //print the string array
+	    for (int i = 0; i < len; i++){
+		printf("%s\n",t[i]);
+	    }
+
+	    //use right to move curr to the right in linked list
+	    right(l);
+	    //view again to update our while condition
+	    temp = view(l);
+	}
+
+	
+
+	
+
+	//commented out code is my old main for old data structure I'll just tweak this slightly for new implementation
+
+	/*
+
+
         char cmd[CMDLINE_MAX];
+
 
         while (1) {
                 char *nl;
                 int retval;
 		pid_t pid;
 
-                /* Print prompt */
+                //Print prompt 
                 printf("sshell$ ");
                 fflush(stdout);
 
-                /* Get command line */
+                //Get command line 
                 fgets(cmd, CMDLINE_MAX, stdin);
 
-                /* Print command line if stdin is not provided by terminal */
+                //Print command line if stdin is not provided by terminal 
                 if (!isatty(STDIN_FILENO)) {
                         printf("%s", cmd);
                         fflush(stdout);
                 }
 
-                /* Remove trailing newline from command line */
+
+                //Remove trailing newline from command line 
                 nl = strchr(cmd, '\n');
                 if (nl)
                         *nl = '\0';
 
-                /* Builtin command */
+                //Builtin command 
                 if (!strcmp(cmd, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         break;
@@ -117,11 +272,6 @@ int main(){
 
 		
 
-		/*
-                retval = system(cmd);
-                fprintf(stdout, "Return status value for '%s': %d\n",
-                        cmd, retval);
-		*/
 
 		
 
@@ -137,12 +287,7 @@ int main(){
 		
 		
 		int fd;
-		/*
-		for (int i = 0; i < len; i++){
-		    printf("%s\n",ds[i]);
-		}
-		*/
-		
+	
 		//printf("%s\n",cmd_2);
 		
 
@@ -204,4 +349,8 @@ int main(){
         }
 
         return EXIT_SUCCESS;
+	*/
+
+	
+	    return 0;
 }
