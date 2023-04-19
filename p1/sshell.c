e0b453aff3636b3406a9efd4869164c997e0e62d
Hanlin Chen <hanlin487@gmail.com>
2023-04-18 20:55:44 -0700

    defined macros for memory sizes when creating objects instead of hardcoding the sizes, cleaned up code and useless comments, sshell can do all simple single commands

--- p1/sshell.c
+++ p1/sshell.c
@@ -7,14 +7,18 @@
 #include <fcntl.h>
 
 #define CMDLINE_MAX 512
+#define BUF_MAX 100
+#define FILE_LEN 16
+#define CMD_ARR_LEN 16
+#define CMD_LEN 32
 
 struct node {
-    bool read_signal; //If the pipe symbol was before this command, set this to true
-    bool write_signal; //If the pipe symbol was after this command, set this to true; if its sorrounded by pipes set both
-    char* file; //File for output redirect (check parse function to see how its set
-    char** command; //String array representing the command (for execvp)
+    bool read_signal; 		//If the pipe symbol was before this command, set this to true
+    bool write_signal; 		//If the pipe symbol was after this command, set this to true; if its sorrounded by pipes set both
+    char* file; 			//File for output redirect (check parse function to see how its set
+    char** command; 		//String array representing the command (for execvp)
     struct node* next; 
-    int length; //length of string array
+    int length; 			//length of string array
 };
 
 //reworking data structures update***
@@ -22,14 +26,14 @@ struct node* createNode(void){
     struct node *n = malloc(sizeof(struct node));
     n -> read_signal = false;
     n -> write_signal = false;
-    n -> file = (char*) malloc(32);
-    n -> command = (char**) malloc(16*sizeof(char*));
+    n -> file = (char*) malloc(FILE_LEN);
+    n -> command = (char**) malloc(CMD_ARR_LEN * sizeof(char*));
    
-	for (int i = 0; i < 16; i++){
-		n -> command[i] = (char*) malloc(32);
+	for (int i = 0; i < CMD_ARR_LEN; i++){
+		n -> command[i] = (char*) malloc(CMD_LEN);
     }
 
-    n -> file = (char*) malloc(32);
+    n -> file = (char*) malloc(FILE_LEN);
     n -> next = NULL;
     n -> length = 0;
     return(n);
@@ -71,7 +75,7 @@ void insert(struct list* l, struct node* n){
 }
 
 void front(struct list* l){
-    if (l -> head == NULL){
+	if (l -> head == NULL){
 		return;
     }
 
@@ -79,7 +83,7 @@ void front(struct list* l){
 }
 
 void right(struct list* l){
-    if (l -> curr == NULL){
+	if (l -> curr == NULL){
 		return;
     }
 
@@ -94,10 +98,10 @@ struct node* view(struct list* l){
 void parse(struct node* a, char* string){
     //struct list *l = malloc(sizeof(struct list));
     char temp[CMDLINE_MAX];
-    a -> command = (char**)malloc(16 * sizeof(char *));
+    a -> command = (char**) malloc(CMD_ARR_LEN * sizeof(char *));
 
     for (int i = 0; i < 16; i++){
-		a -> command[i] = (char*)malloc(32);
+		a -> command[i] = (char*) malloc(CMD_LEN);
     }
 
     strcpy(temp,string);
@@ -106,8 +110,6 @@ void parse(struct node* a, char* string){
     ptr = strtok(temp, " ");
     
 	while (ptr != NULL){
-	//printf("%s\n",ptr);
-
 		if (strcmp(ptr,">") == 0){
 			ptr = strtok(NULL, " ");
 
@@ -162,66 +164,15 @@ void simple_pipe(struct list* l) {
 
 //Data structures are kinda complicated-ish but heres an example below of how to use them
 int main(void){
-	/*
-	//In an actual terminal this would be inputted as ls -l | more, you could then just delimit the | to get 2 strngs
-	char* string = "ls -l | more";
-	char* string2 = "more";
-
-	//Create an empty node
-	struct node* a = createNode();
-
-	//Call parse on that node, the string passed in will be seperated by spaces and turned into a string array of the node (command)
-	//So here, "ls -l" would become an array ["ls","-l"] contained in node a
-	parse(a,string);
-	int len;
-	char** t;
-
-	//for (int i = 0; i < l ; i++){
-	    //printf("%s\n",t[i]);
-	//}
-
-	//Same thing as node a, but for string2
-	struct node* b = createNode();
-	parse(b,string2);
-	
-	//Create a list and then insert both the nodes you just created
-	struct list* l = createList();
-	insert(l,a);
-	insert(l,b);
-
-	//Front to move curr to head
-	front(l);
-
-	//View to return curr node
-	struct node* temp;
-	temp = view(l);
-	while (temp != NULL){
-	    //get length and string array of node
-	    len = getLength(temp);
-	    t = getCommand(temp);
-
-	    //print the string array
-	    for (int i = 0; i < len; i++){
-			printf("%s\n",t[i]);
-	    }
-
-	    //use right to move curr to the right in linked list
-	    right(l);
-
-	    //view again to update our while condition
-	    temp = view(l);
-	}
-
-	//commented out code is my old main for old data structure I'll just tweak this slightly for new implementation
-    */
 	char cmd[CMDLINE_MAX];
 
 	while (1) {
-		char buf[100];
+		char buf[BUF_MAX];
 		char *nl;
 		int retval;
 		int fd;
 		pid_t pid;
+		struct node* new = createNode();
 
 		//Print prompt 
 		printf("sshell$ ");
@@ -238,46 +189,31 @@ int main(void){
 
 		//Remove trailing newline from command line 
 		nl = strchr(cmd, '\n');
+
 		if (nl)
 			*nl = '\0';
+
+		//parse the command line string into the node command object
+		parse(new, cmd);		
+
+		//declare vars for the commands and lengths and add a NULL to the end
+		//of the array for the cmd and args so it's compatible with execvp
+		char** new_cmd = getCommand(new);
+		int new_len = getLength(new);
+		new_cmd[new_len] = NULL;
 
 		//Builtin command 
 		if (!strcmp(cmd, "exit")) {
 			fprintf(stderr, "Bye...\n");
 			break;
 		}
-	
-		/*struct list* a = createList();
-		char** ds = getList(a);
-		char* cmd_2 = ds[0];
-		int len = getLength(a);
-		ds[len] = '\0';
-		char* file = getFile(a);
-		int fd;
-		//printf("%s\n",cmd_2);
-		//execvp(cmd_2,ds);*/
 
-
-		struct node* new = createNode();
-
-		parse(new, cmd);
-		
-		char** new_cmd = getCommand(new);
-		int new_len = getLength(new);
-		new_cmd[new_len] = NULL;
-
-		printf("NODE CMD: ");
-		
-		for (int i = 0; i < new_len; i++){
-			printf("%s ", new_cmd[i]);
-		}
-
-		printf("\n");
-
+		//fork to start the shell process executions
 		pid = fork();
-		
+
+		//CHILD PROCESS
 		if (pid == 0){
-			//child
+			//exit out of child process if commands cannot be executed with execvp
 
 			if (strcmp(new_cmd[0],"cd") == 0){
 				exit(0);
@@ -299,13 +235,13 @@ int main(void){
 			exit(1);
 		}
 		else if (pid > 0){
-
+			//built-in commands
 			if (strcmp(new_cmd[0],"cd") == 0){
 				chdir(new_cmd[1]);
-				printf("%s\n",getcwd(buf,100));
+				printf("%s\n",getcwd(buf, BUF_MAX));
 			}
 			else if (strcmp(new_cmd[0], "pwd") == 0){
-				printf("%s\n", getcwd(buf, 100));
+				printf("%s\n", getcwd(buf, BUF_MAX));
 			}
 
 			waitpid(pid, &retval,0);
