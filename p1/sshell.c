1ffef6cf2095970c6a604cc37119a2409e233eaa
Hanlin Chen <hanlin487@gmail.com>
2023-04-19 15:41:15 -0700

    transferring so I can work on new ver on my desktop, SSHELL.C VERSION NOT FINAL VER, NOT CLEANED OR MERGED, MISSING ERROR HANDLING, ENV VARS, AND FILE REDIRECTION

--- p1/sshell.c
+++ p1/sshell.c
@@ -13,10 +13,8 @@
 #define CMD_LEN 32
 
 struct node {
-    bool read_signal; 		//If the pipe symbol was before this command, set this to true
-    bool write_signal; 		//If the pipe symbol was after this command, set this to true; if its sorrounded by pipes set both
-    char* file; 			//File for output redirect (check parse function to see how its set
-    char** command; 		//String array representing the command (for execvp)
+    char* file; //File for output redirect (check parse function to see how its set
+    char** command; //String array representing the command (for execvp)
     struct node* next; 
     int length; 			//length of string array
 };
@@ -24,8 +22,6 @@ struct node {
 //reworking data structures update***
 struct node* createNode(void){
     struct node *n = malloc(sizeof(struct node));
-    n -> read_signal = false;
-    n -> write_signal = false;
     n -> file = (char*) malloc(FILE_LEN);
     n -> command = (char**) malloc(CMD_ARR_LEN * sizeof(char*));
    
@@ -47,10 +43,15 @@ char** getCommand(struct node* n){
     return n -> command;
 }
 
+char* getFile(struct node* n){
+    return n -> file;
+}
+
 struct list {
     struct node* head;
     struct node* tail;
     struct node* curr;
+    int length;
 };
 
 struct list* createList(void){
@@ -58,6 +59,7 @@ struct list* createList(void){
     a -> tail = NULL;
     a -> head = NULL;
     a -> curr = NULL;
+    a -> length = 0;
     return(a);
 }
 
@@ -66,12 +68,14 @@ void insert(struct list* l, struct node* n){
 		l -> tail = n;
 		l -> head = n;
 		n -> next = NULL;
+		l -> length += 1;
 		return;
     }
 
     l -> tail -> next = n;
     n -> next = NULL;
     l -> tail = n;
+    l -> length +=1;
 }
 
 void front(struct list* l){
@@ -94,9 +98,11 @@ struct node* view(struct list* l){
     return l -> curr;
 }
 
-//Because this parse is a node function I pretty much have to rewrite the strtok code in the main to delimit by "|"
+int getLen(struct list* l){
+    return l -> length;
+}
+
 void parse(struct node* a, char* string){
-    //struct list *l = malloc(sizeof(struct list));
     char temp[CMDLINE_MAX];
     a -> command = (char**) malloc(CMD_ARR_LEN * sizeof(char *));
 
@@ -129,37 +135,59 @@ void parse(struct node* a, char* string){
 		c += 1;
     }
     a -> length = c;
+    a -> command[c] = '\0';
 }
 
-void simple_pipe(struct list* l) {
-	pid_t p1, p2;
-	int fd[2];
-	struct node* temp;
+void pipeline(struct list* l){
+    int length = getLen(l);
+    int fd[2];
+    int prev;
+    int output;
+    char file[FILE_LEN];
+    char** command;
+    pid_t p1;
 
-	front(l);
-	temp = view(l);
-	printf("TEMP CMD: %s\n", *getCommand(temp));
+    prev = STDIN_FILENO;
+    front(l);
 
-	pipe(fd);
+    for (int i = 0; i < length; i++){
+		pipe(fd);
+		command = getCommand(view(l));
+		p1 = fork();
 
-	if(!(p1 = fork())){
-		close(fd[0]);
-		dup2(fd[1], STDOUT_FILENO);
-        close(fd[1]);
+		if (p1 == 0){
+			if (prev != STDIN_FILENO){
+				dup2(prev, STDIN_FILENO);
+				close(prev);
+			}
 
-	}
+			if (i != length - 1){
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[1]);
+			}
+			
+			close(fd[0]);
+			
+			if (getFile(view(l)) != NULL){
+				strcpy(file, getFile(view(l)));
+                output = open(file, O_WRONLY | O_CREAT, 0644);
+				dup2(output, STDOUT_FILENO);
+				close(output);
+			}	
 
-	if (!(p2 = fork())){
-        close(fd[1]);
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
+			execvp(command[0],command);
+			//exit(0);
+		}
 
+		if (prev != STDIN_FILENO){
+			close(prev);
+		}
+
+		close(fd[1]);
+		prev = fd[0];
+		right(l);
     }
-
-	close(fd[0]);
-    close(fd[1]);
     waitpid(p1, NULL, 0);
-    waitpid(p2, NULL, 0);
 }
 
 //Data structures are kinda complicated-ish but heres an example below of how to use them
@@ -193,8 +221,44 @@ int main(void){
 		if (nl)
 			*nl = '\0';
 
+		//Builtin command 
+		if (!strcmp(cmd, "exit")) {
+			fprintf(stderr, "Bye...\n");
+			break;
+		}
+
 		//parse the command line string into the node command object
-		parse(new, cmd);		
+		parse(new, cmd);
+/* ======================== NILESH EDITS START ============================
+		char copy_temp[CMDLINE_MAX];
+		char** store_commands = (char**) malloc(16 * sizeof(char*));
+		char* ptr;
+		int c = 0;
+		
+		for (int i = 0; i < 16; i++){
+		    store_commands[i] = (char*) malloc(32);
+		}
+
+		strcpy(copy_temp,cmd);
+		ptr = strtok(copy_temp,"|");
+
+		while (ptr != NULL){
+		    strcpy(store_commands[c], ptr);
+		    ptr = strtok(NULL,"|");
+		    c+=1;
+		}
+
+		struct list* a = createList();
+		struct node* n;
+
+		for (int i = 0; i < c; i++){
+		    n = createNode();
+		    parse(n, store_commands[i]);
+		    insert(a, n);
+		}
+
+		pipeline(a);
+// ======================== NILESH EDITS END ============================*/
 
 		//declare vars for the commands and lengths and add a NULL to the end
 		//of the array for the cmd and args so it's compatible with execvp
@@ -202,20 +266,14 @@ int main(void){
 		int new_len = getLength(new);
 		new_cmd[new_len] = NULL;
 
-		//Builtin command 
-		if (!strcmp(cmd, "exit")) {
-			fprintf(stderr, "Bye...\n");
-			break;
-		}
-
 		//fork to start the shell process executions
 		pid = fork();
 
 		//CHILD PROCESS
 		if (pid == 0){
+			
 			//exit out of child process if commands cannot be executed with execvp
-
-			if (strcmp(new_cmd[0],"cd") == 0){
+			if (strcmp(new_cmd[0], "cd") == 0){
 				exit(0);
 			}
 
@@ -235,10 +293,11 @@ int main(void){
 			exit(1);
 		}
 		else if (pid > 0){
+
 			//built-in commands
-			if (strcmp(new_cmd[0],"cd") == 0){
+			if (strcmp(new_cmd[0], "cd") == 0){
 				chdir(new_cmd[1]);
-				printf("%s\n",getcwd(buf, BUF_MAX));
+				printf("%s\n", getcwd(buf, BUF_MAX));
 			}
 			else if (strcmp(new_cmd[0], "pwd") == 0){
 				printf("%s\n", getcwd(buf, BUF_MAX));
